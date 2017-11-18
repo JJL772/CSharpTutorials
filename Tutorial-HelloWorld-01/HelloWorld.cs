829a54bd2d2684dcef1eebbf4972e28b79121058
JJL772 <jeremy.lorelli.1337@gmail.com>
2017-11-18 01:09:06 -0800

    Initial check-in of module CSharpTutorials

---
+++ Tutorial-HelloWorld-01/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//This is the project's default namespace
+namespace CSharpTutorials
+{
+	//Main class for the program
+	public class HelloWorld
+	{
+		//The entry point for the program
+		public static void Main(string[] args)
+		{
+
+		}
+	}
+}

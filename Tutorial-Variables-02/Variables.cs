c6fe61d4f5a92bf84e68747353778616bf17a246
Jeremy <jeremy.lorelli.1337@gmail.com>
2017-11-22 16:56:50 -0800

    Create Variables.cs

---
+++ Tutorial-Variables-02/Variables.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Variables
+{
+	public static void Main(string[] args)
+	{
+		int MyInt = 100;
+		string MyString = "Hello, world!";
+		bool MyBool = false;
+		Console.WriteLine("Value of MyInt: " + MyInt);
+	}
+}
